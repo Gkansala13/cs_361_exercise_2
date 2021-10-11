185498865bf65223470f4752625cf2de52c834c1
gkansala13 <kansalag@oregonstate.edu>
2021-10-10 23:23:57 -0700

    reflected principles from clean code chap. 3 and 4

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
- Modify the code to reflect the principles from Clean Code, chapters 3 and 4.
+What is the DRY principle?
+    Don't Repeat Yourself!
+
+You will often hear that functions should be short and do one thing. Does that mean each function should always contain 1 statement? Why or why not?
+    Functions do not always need to be short and do one thing. It's best practice to strive for that, but at times it just makes more sense to have more lines in a function because it requires multiple lines to do its job. It also depends on the style of code you are writing. In genral, keep functions short. Easier to read and understand. Your code tells the story, not dependent on comments.
+
+Imagine that you extract a unit of work into a well-named function, but that function is only called in one place. Your colleague argues, "Why do that? Now when I want to follow the code I have to jump from the call site to the function definition and then back, it breaks up my flow of reading. The function isn't being reused, so just replace the call with the actual statements so it's all right there." The argument has merit. But explain one or two reasons why having the function has its benefits.
+    It is easier to read, as I said above "the code tells the story, not the comments". Who knows, maybe later in the project you will need to reuse that function again. functions look better than a bunch of statements, your code should be beautiful.
--- main.rb
+++ main.rb
@@ -1,35 +1,16 @@
-# Name Mangler
+#Name Mangler
 
-@name = "Johanna Jackson"
+#Cleaned all the pointless comments out
+#Shortened the functions by adding a borg function.
+#Made the program only show the put statments, instead of 'johanna jackson' randomly
 
-# Mangle the name by reversing it or borgifying it
-def modified_name(choose)
-  # Split the name into first and last in an array
-  split_name = @name.split
-  #reversed_twice = split_name.reverse.reverse
-  #mangled_name = reversed_twice << "Mangled"
-  #return mangled_name * 2
-  # Reverse the array of name components so the array is [last, first]
-  reversed_name_components = split_name.reverse
-  # Rejoin the array of name components
-  new_name1 = reversed_name_components.join(' ')
-  # Store the mangled name
-  @name = new_name1
-  puts new_name1
-  if choose
-    # Split the name into last and first in an array
-    split_name2 = new_name1.split
-    # Reverse the array of name components so the array is [last, first]
-    reversed_name_components2 = split_name2.reverse
-    # Add borg to the end of the array
-    reversed_name_components2 << "Borg"
-    # Rejoin the array of name components
-    new_name2 = reversed_name_components2.join(' ')
-    puts new_name2
-    @name = new_name2
-  end
-  return @name
+def first_last_name_swap(first_last_name)
+  first_last_name.split.reverse.join(' ')
 end
 
-puts "New name: #{modified_name(false)}"
-puts "New borg name: #{modified_name(true)}"
+def borg_at_end_of_name(first_last_name)
+  first_last_name_swap(first_last_name) << " Borg"
+end
+
+puts "New name: #{first_last_name_swap("Johanna Jackson")}"
+puts "New borg name: #{borg_at_end_of_name("Johanna Jackson")}"
